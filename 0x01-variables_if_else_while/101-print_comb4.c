7f7ffa6f9d1f85cdadbec50fb0ecc0751203ce1b
TsegaMyeweyni <tsega963@gmail.com>
2021-08-27 13:11:14 -0700

    First commit on Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,15 +2,14 @@
 /**
  * main - main block
  * Description: Print all possible different combinations of 3 digits.
- * Numbers must be separated by ,, followed by a space
- * The three digits must be different
- * 012, 120, 102, 021, 201, 210 are considered the same combination of the three digits 0, 1 and 2
- * Print only the smallest combination of three digits
- * Numbers should be printed in ascending order, with three digits
- * You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
- * You can only use putchar six times maximum in your code
- * You are not allowed to use any variable of type char
- * All your code should be in the main function
+ * Numbers must be separated by commas and a space.
+ * The 3 digits must be different.
+ * 012, 120, 102, 021, 201, 210 are considered the same combination.
+ * print only the smallest combination of 3 digits.
+ * Numbers should be printed in ascending order.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` up to 6 times.
+ * You are not allowed to use any variables of type `char`.
  * Return: 0
  */
 int main(void)
@@ -31,8 +30,8 @@ int main(void)
 
 			if (i < 700)
 			{
-				putchar(44);
-				putchar(32);
+				putchar(34);
+				putchar(22);
 			}
 		}
 	}
