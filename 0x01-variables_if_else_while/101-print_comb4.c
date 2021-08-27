16792ff44752b6946df5477135b34f3815dcdf8f
TsegaMyeweyni <tsega963@gmail.com>
2021-08-27 13:08:05 -0700

    First commit on Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 /**
+ * main - main block
+ * Description: Print all possible different combinations of 3 digits.
  * Numbers must be separated by ,, followed by a space
  * The three digits must be different
  * 012, 120, 102, 021, 201, 210 are considered the same combination of the three digits 0, 1 and 2
