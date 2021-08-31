b1d9af44e66a78c247153afbb48dd728ae412b85
TsegaMyeweyni <tsega963@gmail.com>
2021-08-31 13:05:31 -0700

    task 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 /**
-* main - Entry point
+* main - main
 * write a program tha prints Holberton, followed by a new line.
 * Return: Always (Success)
 */
 int fun(int x, char *p);
 int main(void)
 {
-	char *ch = "_putchar";
+	char ch = "_putchar";
 
-	while (*ch)
+	while (ch)
 	{
 		_putchar(ch);
 		ch ++;
