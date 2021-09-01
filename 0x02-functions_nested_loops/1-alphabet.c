a50cb64d8d4371be26121e0329b5952702b5e6de
TsegaMyeweyni <tsega963@gmail.com>
2021-09-01 12:59:06 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*print_alphabet - a function that prints the alphabet in lowercase.
+*print_alphabet - a function that prints the alphabet in lowercase followed by a new line.
 *
 *Return: Always 0.
 */
