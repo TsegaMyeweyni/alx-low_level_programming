492c5f923e95d9d9c07373c1aba358c9487756e6
TsegaMyeweyni <tsega963@gmail.com>
2021-09-06 12:29:56 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *reset_to_98 - a function that takes a pointer to an int
+ *reset_to_98-a function that takes a pointer to an int.
  * 
- *@*n: int 
+ *@*n: int and update the value to 8. 
  *
  *Return:0
  */
