19f13816379c9bea0ec2faf649bb35ea62a77c16
TsegaMyeweyni <tsega963@gmail.com>
2021-09-06 13:23:57 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strlen - a function that returns the length of a string.
+ *@s: string.
+ *Return: Always 0.
+ */
+int _strlen(char *s)
+{
+f.write(string)
+}
