485be491ffc701420789f06f8e9e0a827a917a58
TsegaMyeweyni <tsega963@gmail.com>
2021-09-06 12:56:38 -0700

    task 2

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,10 +4,13 @@
  *swap_int-swaps the values of two integers.
  *@a:integer a.
  *@b:integer b.
+ *@k:integer value.
  *return: always 0.
  */
 void swap_int(int *a, int *b)
 {
-  swap_int(&a, &b);
+  int k = *a;
+     *a = *b
+     *b = k
   return (0);
 }
