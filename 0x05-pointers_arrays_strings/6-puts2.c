9fe56570271a5b1adc04c68315353b75f25290be
TsegaMyeweyni <tsega963@gmail.com>
2021-09-07 11:48:31 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * puts2 - a function that prints every other character of a string.
- * @str: string.
- * Return: No.
+ * puts2 - prints one char out of 2 of a string.
+ * @str: input string.
+ * Return: no return.
  */
 void puts2(char *str)
 {
