33ccbbc934db69dafe2d5d884b271b9a2d2126f0
TsegaMyeweyni <tsega963@gmail.com>
2021-09-15 12:59:21 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -13,7 +13,7 @@ int _sqrt_recursion(int n)
 }
 
 /**
- * halp - helper function to solve _sqrt_recursion
+ * help - helper function to solve _sqrt_recursion
  * @c: number to determine if square root
  * @i: incrementer to compare against `c`
  * Return: square root if natural square root, or -1 if none found
